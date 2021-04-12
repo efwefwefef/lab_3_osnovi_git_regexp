99ffc2014e2fdb818c004e9218904c5073a384fb
nik <nik@yandex.ru>
2021-04-12 16:23:51 +0300

    доработка функции main

--- src/lab_3.java
+++ src/lab_3.java
@@ -3,7 +3,9 @@ public class lab_3 {
 
     public static void main(String[] args)
     {
-     String[] mm = FileParser.GeTextStats();
+        String[] mm = FileParser.GeTextStats();
 
+        for(String i : mm)
+            System.out.println(i);
     }
 }
