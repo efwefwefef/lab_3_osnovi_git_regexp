258dd83916659fffbfff73ea85c792c076027fcf
nik <nik@yandex.ru>
2021-04-12 15:45:50 +0300

    добавил регулярные выражения для определения негативных слов и просто слов в тексте

--- src/FileParser.java
+++ src/FileParser.java
@@ -14,9 +14,14 @@ import  java.util.regex.Pattern;
 public class FileParser {
     static void GetAllPhrases()
     {
-        String str = "we11z11iuhf10-88fg красивый eefugeu12-78ggc смешной bcbcb34-427gf";
-        Pattern pp = Pattern.compile("[ ,.][а-яА-я]{1,}((ой)|(ый))[ ,.]");
-        Matcher m = pp.matcher(str);
+        String text = "собака. негативынй,некрасивый, дом. Зоопарк, бы";
+
+        String neg_words_reg_expr = "([ .,]смерть)|([ .,]боль)|([ .,]тоска)|([ .,]горе)|([ .,](не)[a-яА-я]+)"; //слова с негативным окрасом
+        String word_reg_expr = "[ .,][a-яА-я]{3,}"; //все слова, кроме 1 и слов длиной меньшей 3
+        Pattern neg_pattern = Pattern.compile(neg_words_reg_expr);
+        Pattern all_words = Pattern.compile(word_reg_expr);
+
+        Matcher m = neg_pattern.matcher(text);
         while (m.find()) {
             String tmp = m.group();
             System.out.println(tmp);
